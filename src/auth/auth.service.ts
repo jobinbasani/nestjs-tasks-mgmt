320a459addb69363c92fb74f8f5f240d9b21f868
Jobin Basani <jobinbasani@gmail.com>
2020-03-30 22:02:30 -0300

    Add auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { UserRepository } from './user.repository';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository:UserRepository
+    ){}
+}

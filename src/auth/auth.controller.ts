c3c87c19aadfbcd3dac3770ee386cbda71e5e321
Jobin Basani <jobinbasani@gmail.com>
2020-03-31 21:26:42 -0300

    Add signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService:AuthService){}
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentialsDto:AuthCredentialsDto): Promise<void>{
+        return this.authService.signUp(authCredentialsDto);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,4 +9,8 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private userRepository:UserRepository
     ){}
+
+    async signUp(authCredentialsDto:AuthCredentialsDto): Promise<void>{
+        return this.userRepository.signUp(authCredentialsDto);
+    }
 }

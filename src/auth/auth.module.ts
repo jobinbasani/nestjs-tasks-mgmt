28b95620ce9f687ea87a59cc60081cfc96cec4d2
Jobin Basani <jobinbasani@gmail.com>
2020-04-09 21:54:59 -0300

    Add config mgmt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,9 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import {JwtModule} from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
@@ -13,9 +16,9 @@ import { PassportModule } from '@nestjs/passport';
       defaultStrategy: 'jwt',
     }),
     JwtModule.register({
-      secret: 'topSecret51',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions:{
-        expiresIn: 3600,
+        expiresIn: jwtConfig.expiresIn,
       }
     }),
     TypeOrmModule.forFeature([UserRepository])

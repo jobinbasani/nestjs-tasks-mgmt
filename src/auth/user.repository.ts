b8063e6161aeff6e5dd4e33e108be9680468523d
Jobin Basani <jobinbasani@gmail.com>
2020-04-12 16:10:37 -0300

    Switch to bcryptjs

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,7 +2,7 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { Repository, EntityRepository } from "typeorm";
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{

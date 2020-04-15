361d793c6bafe60a6aa225f32dff14a0963b0cc1
Jobin Basani <jobinbasani@gmail.com>
2020-04-14 22:10:27 -0300

    Add test cases

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,5 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
-import { User } from 'src/auth/user.entity';
+import { NotFoundException, Delete } from '@nestjs/common';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 import {Test} from '@nestjs/testing';
@@ -12,6 +11,8 @@ const mockUser = {id:100, username: 'TestUser'};
 const mockTaskRepository = () => ( {
     getTasks: jest.fn(),
     findOne: jest.fn(),
+    createTask: jest.fn(),
+    delete: jest.fn(),
 });
 
 describe('TaskService', ()=>{
@@ -56,6 +57,46 @@ describe('TaskService', ()=>{
         it('throws an error for invalid task', () => {
             tasksRepository.findOne.mockResolvedValue(null);
             expect(tasksService.getTaskById(1, mockUser)).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    describe('createTask', ()=>{
+        it('create new task', async () =>{
+            expect(tasksRepository.createTask).not.toHaveBeenCalled();
+            const mockTask = {title:'NewTask', descr:'NewDescr'};
+            tasksRepository.createTask.mockResolvedValue('ReturnValue');
+            const result = await tasksService.createTask(mockTask, mockUser);
+            expect(tasksRepository.createTask).toHaveBeenCalled();
+            expect(result).toEqual('ReturnValue');
+            expect(tasksRepository.createTask).toHaveBeenCalledWith(mockTask,mockUser);
+        });
+    });
+
+    describe('deleteTaskById', () => {
+        it('delete existing task', async () => {
+            tasksRepository.delete.mockResolvedValue({affected: 1});
+            expect(tasksRepository.delete).not.toHaveBeenCalled();
+            await tasksService.deleteTaskById(1,mockUser);
+            expect(tasksRepository.delete).toHaveBeenCalledWith({id:1, userId:mockUser.id});
+        });
+        it('throws an exception when deleting', () => {
+            tasksRepository.delete.mockResolvedValue({affected: 0});
+            expect(tasksService.deleteTaskById(1,mockUser)).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    describe('updateTaskStatus', ()=>{
+        it('update task status', async ()=>{
+            const save = jest.fn().mockResolvedValue(true);
+            tasksService.getTaskById = jest.fn().mockResolvedValue({
+                status: TaskStatus.OPEN,
+                save,
+            });
+            expect(tasksService.getTaskById).not.toHaveBeenCalled();
+            const result = await tasksService.updateTaskStatus(1,TaskStatus.DONE, mockUser);
+            expect(tasksService.getTaskById).toHaveBeenCalled();
+            expect(save).toHaveBeenCalled();
+            expect(result.status).toEqual(TaskStatus.DONE);
         });
     });
 });
